27b0a2dad8088e89564797c4ee42f82c8e90bdc3
Adeeb2022 <adeebsamad01@gmail.com>
2023-02-13 01:33:04 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
-}
-
-document.addEventListener('DOMContentLoaded', init);
+	const form = document.querySelector('form')
+	const input = document.querySelector('#searchByID');
+	
+	form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	
+	fetch(`http://localhost:3000/movies/${input.value}`)
+	.then(response => response.json())
+	.then(data => {
+        
+	const title = document.querySelector('#movieDetails h4');
+	const summary = document.querySelector('#movieDetails p');
+	
+	title.innerText = data.title;
+	summary.innerText = data.summary;
+	});
+	});
+	}
+	
+	document.addEventListener('DOMContentLoaded', init);
